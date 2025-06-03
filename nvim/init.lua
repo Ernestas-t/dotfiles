2e11af2fc244a12fb1d3ca381bbe90c497018653
Ernestas Tautkus <40767813+Ernestas-t@users.noreply.github.com>
2025-06-03 16:45:05 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -178,7 +178,7 @@ require("lazy").setup({
 
 	-- Obsidian integration
 	{
-		"epwalsh/obsidian.nvim",
+		"obsidian-nvim/obsidian.nvim",
 		version = "*",
 		lazy = true,
 		ft = "markdown",
@@ -186,13 +186,52 @@ require("lazy").setup({
 		opts = {
 			workspaces = {
 				{
-					name = "main",
+					name = "personal",
 					path = "~/Documents/Obsidian",
+				},
+				{
+					name = "Work",
+					path = "/mnt/c/Users/ernestas.tautkus/Documents",
 				},
 			},
 			follow_url_func = function(url)
 				vim.ui.open(url)
 			end,
+		},
+	},
+
+	-- Image display in Neovim
+	{
+		"3rd/image.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-treesitter/nvim-treesitter",
+				build = ":TSUpdate",
+			},
+		},
+		opts = {
+			backend = "kitty",
+			processor = "magick_rock", -- or "magick_cli" if you have ImageMagick installed
+			integrations = {
+				markdown = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = true,
+					only_render_image_at_cursor = false,
+					filetypes = { "markdown", "vimwiki" },
+				},
+			},
+			max_width = 100,
+			max_height = 12,
+			max_width_window_percentage = nil,
+			max_height_window_percentage = 50,
+			window_overlap_clear_enabled = false,
+			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+			editor_only_render_when_focused = false,
+			tmux_show_only_in_active_window = false,
+			hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
 		},
 	},
 
